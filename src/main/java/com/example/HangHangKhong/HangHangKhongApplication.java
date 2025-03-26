14f176b83638e8e22af2789a22bbe33330266c88
HuynFZ <nguyenvanmanhhuy1@gmail,com>
2025-03-26 21:16:34 +0700

    add login-res

---
+++ src/main/java/com/example/HangHangKhong/HangHangKhongApplication.java
@@ -0,0 +1,13 @@
+package com.example.HangHangKhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangHangKhongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangHangKhongApplication.class, args);
+	}
+
+}
